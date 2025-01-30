7c5998d24f1d89f9815c397f71b71e8a0caf64a4
yu hsiang <lin@test.com>
2025-01-30 18:09:05 +0800

    #6 切版區塊 個人經歷&footer

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,24 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col text1">test</div>
-      <div class="col text2">test</div>
-      <div class="col">test</div>
-      <div class="col">test</div>
+  <!-- 個人經歷 -->
+  <div class="work-exp-area">
+    <div class="container py-4">
+      <div class="row">
+        <div class="col bold mb-3">個人經歷</div>
+      </div>
+      <div class="row work-exp-card">
+        <div class="col-2 date">2018-2020</div>
+        <div class="col-10 description">做了OOO使用了技術</div>
+      </div>
+      <div class="row work-exp-card">
+        <div class="col-2 date">2018-2020</div>
+        <div class="col-10 description">做了OOO使用了技術</div>
+      </div>
     </div>
   </div>
+  <!-- ./個人經歷 -->
+  <!-- footer -->
+  <div class="d-flex justify-content-center align-items-center footer-area">
+    Yuhsiang copyright
+  </div>
+  <!-- ./footer -->
 </template>
