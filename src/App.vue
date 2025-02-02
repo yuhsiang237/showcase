50da703f2b90b85d6a4d5794d29fd50a2a9d000c
yu hsiang <lin@test.com>
2025-02-02 12:04:03 +0800

    #6 版面調整

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
 <template>
-  <div class="banner"></div>
+  <div class="banner">
+    <div
+      class="d-flex text-center align-items-center justify-content-center"
+      style="height: 100vh; color: #fff"
+    >
+      <div>
+        <div
+          style="font-size: 3.8rem; font-weight: 100; letter-spacing: 0.2rem"
+        >
+          LAX's Resume
+        </div>
+        履歷 | 作品 | 展示
+      </div>
+    </div>
+  </div>
   <RouterView />
 </template>
 <script>
