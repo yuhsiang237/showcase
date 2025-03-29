05c572a6850b76dfb63e2c18dbf824e7ee6d1ba2
yu hsiang <lin@test.com>
2025-03-29 19:21:27 +0800

    v2 bg

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,7 @@
 <template>
-  <div
-    class="banner"
-    :style="{
-      backgroundImage:
-        'linear-gradient(rgba(0, 0, 0, 0.15), rgba(0, 0, 0, 0.15)), url(' +
-        backgroundImage +
-        ')',
-    }"
-  >
-    <div
-      class="d-flex text-center align-items-center justify-content-center"
-      style="height: 83vh; color: #fff"
-    >
-      <div class="wow fadeIn" data-wow-duration="2s">
-        <div
-          style="font-size: 3.8rem; font-weight: 100; letter-spacing: 0.2rem"
-        >
-          LAX's Resume
-        </div>
-        履歷 | 作品 | 展示
-      </div>
-    </div>
-  </div>
+<div><img style="width:100%"
+:src="backgroundImage"/></div>
+
   <RouterView />
   <GoTop />
 </template>
