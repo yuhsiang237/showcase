1051b7e205d43b5efdc1817ecaf3d525150f6137
yu hsiang <lin@test.com>
2025-03-30 13:41:05 +0800

    更新文案

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,7 +8,7 @@
           <div class="title1 mb-3">自我介紹</div>
           <div
             data-wow-duration="10s"
-            class="medata wow animate__animated animate__fadeIn"
+            class="pr-3 medata wow animate__animated animate__fadeIn"
             v-html="meData"
           ></div>
         </div>
@@ -175,14 +175,17 @@ export default {
     const demo = ref(Demo);
     const modal = ref(null);
     const meData = ref(
-      `你好，我是郁翔(YU HSIANG)<br/><br/>
-      在過往工作中，發覺仍然喜歡做與圖像相關的工作。<br/>
-      因此，職涯方向為前端軟體工程師。<br/>
-      並以能穩定開發、產出為主，目前參與過專案皆於時程內完成。<br/>
-      <br/>
-      對於新事物掌握速度快，因資訊產業迭代速度快，對於新出之框架或衍伸服務，可以在2週內熟悉至可上手程度。(依過往工作經歷)
-      <br/>
-      <br/>
+      `
+你好，我是郁翔 (YU HSIANG) <br/>
+
+在過去參與的工作專案中，仍對做圖像工作感興趣。<br/>因此，將職涯方向定為前端工程師。 <br/>
+並以能穩定開發、產出為主，目前參與過專案皆於時程內完成。 <br/>
+<br/>
+對於新事物掌握速度快，對新出框架或衍伸服務，可以在2週內熟悉至可上手程度。(依過往工作經歷)<br/>
+
+<br/>
+在結束台北緯創軟體駐點後，回高雄找了一卡通工作，主要負責「一卡通iPASS MONEY」的底層專案，實際工作後發現為DBA性質，因此後續評估不太適合。期間亦嘗試其他產業多方探索，如日商藏壽司、Uber eat等，並梳理好職涯方向。<br/>
+<br/>
       <a target="_blank" href="https://github.com/yuhsiang237"><i class="fa-brands fa-github"></i> Github - yuhsiang237</a>
       <br/>
       <a target="_blank" href="https://yuhsiang237.github.io/"><i class="fa-solid fa-earth-americas"></i> 程式筆記網站</a>
