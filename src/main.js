b3191942dd9a6e7854199132cfae2b684d2b255b
yu hsiang <lin@test.com>
2025-06-02 23:21:29 +0800

    Add Lazy loading for Img

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import "./assets/style/main.scss";
 
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import VueLazyLoad from "vue3-lazyload";
 import WOW from "wow.js";
 import App from "./App.vue";
 import router from "./router";
@@ -10,7 +11,9 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-
+app.use(VueLazyLoad, {
+  // options...
+});
 // 初始化 WOW
 new WOW().init();
 
