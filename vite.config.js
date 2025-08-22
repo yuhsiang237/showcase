415e382d93bc5d616097932ef145db215b8c2a19
yu hsiang <lin@test.com>
2025-08-22 23:11:41 +0800

    add img compress viteImagemin

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import viteImagemin from 'vite-plugin-imagemin';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,6 +11,18 @@ export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    viteImagemin({
+      gifsicle: { optimizationLevel: 7, interlaced: false },
+      optipng: { optimizationLevel: 7 },
+      mozjpeg: { quality: 65 },
+      pngquant: { quality: [0.65, 0.8], speed: 4 },
+      svgo: {
+        plugins: [
+          { name: 'removeViewBox' },
+          { name: 'removeEmptyAttrs', active: false },
+        ],
+      },
+    }),
   ],
   resolve: {
     alias: {
